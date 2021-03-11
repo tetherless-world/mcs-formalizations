713b833542de4bd7b87ee45ab29ccd6cf48fddcc
Gretchen Forbush <forbug@rpi.edu>
2021-03-10 21:44:32 -0500

    Updated metrics file for new files

--- txt_classification/metrics.py
+++ txt_classification/metrics.py
@@ -4,8 +4,10 @@ from mcs_formalizations.path import DATA_DIR_PATH
 
 initials = ["M", "H", "A", "R", "G"]
 
+annotators = ["Minor", "Henrique", "Alice", "Rebecca", "Gretchen"]
 
-def get_scores(df):
+
+def get_scores_original(df):
 
     for init in initials:
 
@@ -29,11 +31,35 @@ def get_scores(df):
             print(metrics.classification_report(y_test, y_pred))
 
 
+def get_scores_new(df):
+
+    for name in annotators:
+
+        y_test = df[name]
+
+        others = [i for i in annotators if i != name]
+
+        for other in others:
+            print(name + " vs. " + other)
+
+            y_pred = df[other]
+
+            print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+            print("Balanced Accuracy:", metrics.balanced_accuracy_score(y_test, y_pred))
+            print("Precision:", metrics.precision_score(y_test, y_pred))
+            print("Recall:", metrics.recall_score(y_test, y_pred))
+            print("F1 score", metrics.f1_score(y_test, y_pred))
+
+            tn, fp, fn, tp = metrics.confusion_matrix(y_test, y_pred).ravel()
+            print(tn, fp, fn, tp)
+            print(metrics.classification_report(y_test, y_pred))
+
+
 if __name__ == "__main__":
 
-    with open(DATA_DIR_PATH / "categorization/Binary-Time.csv") as csv_file:
+    with open(DATA_DIR_PATH / "categorization/Time-Binary-1.csv") as csv_file:
         df = pd.read_csv(csv_file)
 
         df.drop(df.tail(1).index, inplace=True)
 
-        get_scores(df)
+        get_scores_new(df)
